050fd13d197b75babf418cc327e8b08eac06d5b8
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-09-08 08:51:25 +0200

    Project created

---
+++ src/test/java/com/rcrdev/rcrstore/RcrStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcrdev.rcrstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcrStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
