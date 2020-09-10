7b62c77f45427d5ec9b186e24179abaf3c046c1e
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-09-10 09:33:35 +0200

    Category Instantiation Operation

--- src/main/java/com/rcrdev/rcrstore/RcrStoreApplication.java
+++ src/main/java/com/rcrdev/rcrstore/RcrStoreApplication.java
@@ -1,13 +1,32 @@
 package com.rcrdev.rcrstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RcrStoreApplication {
+import com.rcrdev.rcrstore.domain.Category;
+import com.rcrdev.rcrstore.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class RcrStoreApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RcrStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informatics");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
